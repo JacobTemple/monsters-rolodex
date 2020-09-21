9e7c353588bfe9a04ed525c753b7f6f4157ccf2b
JacobTemple <32564117+JacobTemple@users.noreply.github.com>
2020-09-21 16:54:28 -0700

    Updates

---
+++ precache-manifest.a9140417ac47c27798d54c220bebad3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be428901854fc389283f873d51f68a3a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b4ee05537e9a97b2298b",
+    "url": "/monsters-rolodex/static/css/main.532613e8.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4ee05537e9a97b2298b",
+    "url": "/monsters-rolodex/static/js/main.08cc77a2.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
